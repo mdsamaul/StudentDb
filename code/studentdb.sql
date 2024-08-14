4f5ed23351db4ad894742dda1bb695c1590b0b91
Md Samaul <mdsamal843@gmail.com>
2024-08-14 11:19:30 +0600

    added all store procedure

--- code/studentdb.sql
+++ code/studentdb.sql
@@ -130,4 +130,123 @@ EXEC AddStudent
 select * from Students;
 go
 
---
+--insert Course table
+INSERT INTO Courses (CourseName, Description, Credits)
+VALUES 
+('C Programming', 'Introduction to the C programming language.', 3),
+('C++ Programming', 'Advanced study of object-oriented programming using C++.', 4),
+('C# Programming', 'Overview of the C# language features.', 3);
+go
+
+--Enrollments store procedure
+create proc EnrollStudent(
+	@StudentID int,
+    @CourseID int,
+    @EnrollmentDate date,
+    @Grade char(2)
+)
+as
+begin
+	if not exists(select 1 from Students where @StudentID=StudentID)
+	begin
+		raiserror('not avaible student',16,1)
+	end
+	else if not exists(select 1 from Courses where @CourseID=CourseID)
+	begin
+		raiserror('not avaible Course',16,1)
+	end
+	else if @EnrollmentDate > getdate()
+	begin
+		raiserror('Invalie date',16,1)
+	end
+	else
+	begin
+		insert into Enrollments(StudentID,CourseID,EnrollmentDate,Grade)
+		values(@StudentID,@CourseID,@EnrollmentDate,@Grade);
+	end
+end;
+go
+--EnrollStudent insert values
+
+exec EnrollStudent
+	@StudentID = 1,
+	@CourseID=1,
+	@EnrollmentDate='2011-01-01',
+	@Grade = 'A';
+	go
+
+
+select * from Enrollments;
+go
+
+--add exam proc
+create proc AddExam(
+	@CourseID int,
+	@ExamDate date,
+	@TotalMarks int
+)
+as
+begin
+	if not exists(select 1 from Courses where @CourseID=CourseID)
+	begin
+		raiserror('not avaible Course',16,1)
+	end
+	else if @ExamDate > getdate()
+	begin
+		raiserror('Invalie date',16,1)
+	end
+	else if @TotalMarks < 0
+	begin
+		raiserror('mark can not negetiv value',16,1)
+	end
+	else
+	begin
+		insert into Exams(CourseID, ExamDate,TotalMarks)
+		values(@CourseID, @ExamDate,@TotalMarks);
+	end
+end;
+go
+
+exec AddExam @CourseId=1,
+	@ExamDate='2022-01-01',
+	@TotalMarks = 99;
+go
+
+--select 
+select * from Exams;
+go
+
+--proc result table
+create proc AddResult(
+	@StudentID int,
+    @ExamID int,
+    @MarksObtained float
+)
+as
+begin
+	if not exists(select 1 from Students where @StudentID= StudentID)
+		begin
+			raiserror ('Student not avbile',16,1);
+		end
+	else if not exists(select 1 from Exams where @ExamID= ExamID)
+		begin
+			raiserror ('Exam not avbile',16,1);
+		end
+	else if @MarksObtained <0
+		begin
+			raiserror('mark can not negetive value',16,1);
+		end
+	else
+		begin
+			insert into Results(ExamID,MarksObtained)
+			values(@ExamID,@MarksObtained)
+		end
+end;
+go
+
+exec AddResult @StudentId=1,
+@ExamId=1,@marksObtained=2.2;
+go
+
+--update result on trigger
+--create trigger
